7027aca91bc751ff1ebda733c1371a77453bf556
0xKubitus <n.cubilier@gmail.com>
2022-08-31 10:26:47 +0200

    push pour correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users,
+               controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations'
+               }
+    get '/member-data', to: 'members#show'
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+    resources :articles
 
-  resources :articles
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "articles#index"
+    # Defines the root path route ("/")
+    root "articles#index"
 end
